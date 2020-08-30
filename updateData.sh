82ba82bbc9b89864a50de0c2eb65cc96558b5478
Krzysztof Zbicinski <zbicin@users.noreply.github.com>
2020-08-30 19:37:07 +0200

    Improve updateData

--- updateData.sh
+++ updateData.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-vnstat -i wwp0s26u1u3i4 -d --json | python -m json.tool > ./data.json
+cd "$(dirname "$0")"
+vnstat -i wwp0s26u1u3i4 -d --json | python -m json.tool > data.json
 git add data.json
 git commit -m 'Update data' --author="crontab <crontab@example.com>"
 git push origin master
